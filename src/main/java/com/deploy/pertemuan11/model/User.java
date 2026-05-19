0ba025a396fb7a113fece2e6d0d4976226a209ff
galang48 <galangyudha1945@gmail.com>
2026-05-19 10:21:28 +0700

    menambahkan entitas User beserta relasi one-to-one dengan Profile

--- src/main/java/com/deploy/pertemuan11/model/User.java
+++ src/main/java/com/deploy/pertemuan11/model/User.java
@@ -1,4 +1,26 @@
 package com.deploy.pertemuan11.model;
 
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profile profile;
 }
